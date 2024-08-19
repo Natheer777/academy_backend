de49b70ccbb40d89b14ff14ef4e90a7024abdd61
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-08-19 14:52:06 +0300

    dd

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const router = require("./router/route");
 const bodyParser = require("body-parser");
 const path = require("path");
 const mysql = require('mysql2');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = ['https://natheer777.github.io', 'http://localhost:5173/academy/about'];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(router);
 
 app.use(express.static(path.join(__dirname, 'public')));
