8266e9185e4a056d3ad41b81e5134d8d291cd8e2
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-10-14 07:39:12 +0300

    W

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:5500",
+      "https://academy-backend-pq91.onrender.com"
     ];
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
