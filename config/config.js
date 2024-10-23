94c602c9778d1acb4160511407e8f0802130c371
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-10-23 06:03:49 +0300

    aa

--- config/config.js
+++ config/config.js
@@ -2,18 +2,19 @@ const mysql = require('mysql2/promise');
 
 // إعداد الاتصال بقاعدة البيانات
 const db = mysql.createPool({
-    host: 'b6cg8mpnnxei6eygavd5-mysql.services.clever-cloud.com',
-    port: 3306,
-    user: 'uo3sgblsubaqgefa',
-    database: 'b6cg8mpnnxei6eygavd5',
-    password: '3xtHxqDjQCw0JMDpY1NN',
+    // host: 'b6cg8mpnnxei6eygavd5-mysql.services.clever-cloud.com',
+    // port: 3306,
+    // user: 'uo3sgblsubaqgefa',
+    // database: 'b6cg8mpnnxei6eygavd5',
+    // password: '3xtHxqDjQCw0JMDpY1NN',
 
-    // host: 'srv621.hstgr.io',
-    // user: 'u229294786_UKHrI',
-    // password: 'Sawa2020!',
-    // database: 'u229294786_JZfqq',
+    host: 'srv621.hstgr.io',
+    password: 'Sawavps2024%',
+    user: 'u229294786_T7V3N',
+    database: 'u229294786_pGFtY',
 
 });
+
 
 
 // اختبار الاتصال
