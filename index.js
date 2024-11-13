de3b5578744fa8e978e5d15bb28a16d5b0c41359
Natheer777 <111202840+Natheer777@users.noreply.github.com>
2024-11-13 15:00:30 +0300

    ssss

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // إعدادات Express
 app.use(bodyParser.json());
